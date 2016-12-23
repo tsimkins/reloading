569082f18907006973438377b13a284a189eb418
Tim Simkins <tim.simkins@tandj.net>
2016-12-23 17:22:52 -0500

    Updated url routes for new Django version.

--- reloading/urls.py
+++ reloading/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^reloading/admin/', include(admin.site.urls)),
-    url(r'^reloading/$', 'reloading.views.listing', name='listing_view'),
-    url(r'^reloading/caliber/(?P<slug>[a-z0-9\\-]+)$', 'reloading.views.byCaliber', name='caliber_view'),
-)
+urlpatterns = [
+    url(r'^reloading/admin/', admin.site.urls),
+    url(r'^reloading/*$', views.listing, name='listing_view'),
+    url(r'^reloading/caliber/(?P<slug>[a-z0-9\\-]+)$', views.byCaliber, name='caliber_view'),
+]
 
 
 
